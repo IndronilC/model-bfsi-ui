8f2e8c320e1728793fa10121d148d20679834f45
Thanaraj-914 <110971256+Thanaraj-914@users.noreply.github.com>
2023-06-28 23:57:37 +0530

    Set Customer Data

--- src/Utils/common.tsx
+++ src/Utils/common.tsx
@@ -39,4 +39,27 @@ const Data = {
     Data.phoneNumber = data.mobileNumber;
   };
   
+  export const setCustomerdata = (data:any) => {
+    Data.firstName = data.firstName;
+    Data.lastName = data.lastName;
+    Data.dateOfBirth = data.dob;
+    Data.address1 = data.address1;
+    Data.address2 = data.address2;
+    Data.state = data.state;
+    Data.city = data.city;
+    Data.zipcode = data.zipcode;
+    Data.fullName = data.firstName + " " + data.lastName;
+    Data.address =
+      data.address1 +
+      ", " +
+      data.address2 +
+      " ," +
+      data.state +
+      ", " +
+      data.city +
+      ", " +
+      data.zipcode;
+  };
+  
+
   
