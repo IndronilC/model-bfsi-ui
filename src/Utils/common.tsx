122adfa4ceb0b7a0c647116c0e4ef77e384e17ae
Thanaraj-914 <110971256+Thanaraj-914@users.noreply.github.com>
2023-06-28 23:55:19 +0530

    Create a one common TS file and all input given as a one object

---
+++ src/Utils/common.tsx
@@ -0,0 +1,34 @@
+//Data object which fetches data from all the pages
+
+const Data = {
+    fullName: "",
+    address: "",
+    email: "",
+    panNumber: "",
+    phoneNumber: "",
+    aadharNumber: "",
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    address1: "",
+    address2: "",
+    state: "",
+    city: "",
+    zipcode: "",
+    addressProofType: "",
+    idProofType: "",
+    securityQuestionsId: "",
+    securityQuestionsId2: "",
+    securityQuestionsId3: "",
+    Question1: "",
+    Question2: "",
+    Question3: "",
+    Answer1: "",
+    Answer2: "",
+    Answer3: "",
+  };
+  
+  export const getData = () => {
+    return Data;
+  };
+  
