4f22ae3b27f349223342a09189a0dd225bf8d669
Thanaraj-914 <110971256+Thanaraj-914@users.noreply.github.com>
2023-06-28 22:58:45 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import First from './components/onboard/First';
+import PersonalDetails from './components/PersonalDetails';
+import CustomerIdentification from './components/CustomerIdentification';
+import KYC from './components/KYC';
+import SecurityQuestions from './components/SecurityQuestions';
+import SummaryPage from './components/SummaryPage'
+import Final from './components/Final';
+import Sidenav from './shared/sidenav/sidenav';
+import Header from './shared/header/header';
+import Footer from './shared/footer/footer';
+function App() {
+  return (
+    <div className="App">
+      
+        <BrowserRouter>
+        <Sidenav />
+        <Header/>
+        <Footer />
+        <Routes>
+          <Route  path="/Personaldetails" element={<PersonalDetails/>} /> 
+          <Route  path="/First" element={<First/>} /> 
+          <Route  path="/CustomerIdentification" element={<CustomerIdentification/>} /> 
+          <Route  path="/KYC" element={<KYC/>} />  
+          <Route  path="/SecurityQuestions" element={<SecurityQuestions/>} />  
+          <Route  path="/SummaryPage" element={<SummaryPage/>} /> 
+          <Route  path="/Final" element={<Final/>} />  
+         
+        </Routes>    
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
