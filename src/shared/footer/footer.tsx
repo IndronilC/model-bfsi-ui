8d2812a9006efbc7544d31678154de99079afc29
Thanaraj-914 <110971256+Thanaraj-914@users.noreply.github.com>
2023-06-29 14:37:27 +0530

    Pagination fuctionally working for all the pages

--- src/shared/footer/footer.tsx
+++ src/shared/footer/footer.tsx
@@ -9,37 +9,51 @@ export default function Footer() {
     
  
   const location = useLocation();
-  var currentPage ="";
+  //var currentPage ="";
 
-   currentPage = location.pathname.slice(1);
+  const currentPage = location.pathname.slice(1);
+   //let Idstate="";
+  // useEffect(() => {
+  //   console.log();
+  //   const myClass: HTMLInputElement | null = document.querySelector("." + location.pathname.slice(1)) as HTMLInputElement;
+  //   console.log(myClass);
+
+  //   if (myClass) {
+  //     //Idstate="";
+  //       myClass.id = "active";
+  //       Idstate=myClass.id;
+  //       console.log(myClass);
+  //   }
+
+  //   // document.querySelector("." + location.pathname.slice(1)).id = "active";
+  //   console.log();
+  // }, [currentPage]);
 
   useEffect(() => {
-    console.log();
-    const myClass: HTMLInputElement | null = document.querySelector("." + location.pathname.slice(1)) as HTMLInputElement;
+    const buttons = document.querySelectorAll(".pagination-btn");
 
-    if (myClass) {
-        myClass.id = "active";
-    }
-
-    // document.querySelector("." + location.pathname.slice(1)).id = "active";
-    console.log();
+    buttons.forEach((button) => {
+      const span = button.querySelector("span");
+      const className = span?.className || "";
+      const isActive = className === currentPage ? "active" : "";
+      span?.setAttribute("id", isActive);
+    });
   }, [currentPage]);
-
 
   return (
     <div>
           <div className="pagination">
             <button className="pagination-btn">
-              <span className="PersonalDetails" id=""></span>
+              <span className="PersonalDetails" ></span>
             </button>
             <button className="pagination-btn ">
-              <span className="CustomerIdentification" id=""></span>
+              <span className="CustomerIdentification" ></span>
             </button>
             <button className="pagination-btn ">
-              <span className="KYC" id=""></span>
+              <span className="KYC" ></span>
             </button>
             <button className="pagination-btn">
-              <span className="SecurityQuestions" id=""></span>
+              <span className="SecurityQuestions" ></span>
             </button>
           </div>
     </div>
